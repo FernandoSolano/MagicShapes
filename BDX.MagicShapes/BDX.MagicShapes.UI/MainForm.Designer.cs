d97519675ae45b5fdc0ae79b6002a0a4dc09e6bb
Fernando Solano <fer_odi10@hotmail.com>
2018-07-24 16:55:38 -0600

    First Commit, visual rectangle drawing functionality added

---
+++ BDX.MagicShapes/BDX.MagicShapes.UI/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace BDX.MagicShapes.UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonZoom = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.canvas, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(984, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.buttonZoom);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 532);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(978, 26);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonZoom
+            // 
+            this.buttonZoom.Location = new System.Drawing.Point(0, 0);
+            this.buttonZoom.Name = "buttonZoom";
+            this.buttonZoom.Size = new System.Drawing.Size(75, 23);
+            this.buttonZoom.TabIndex = 0;
+            this.buttonZoom.Text = "Zoom In";
+            this.buttonZoom.UseVisualStyleBackColor = true;
+            this.buttonZoom.Click += new System.EventHandler(this.buttonZoom_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(3, 3);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(978, 523);
+            this.canvas.TabIndex = 1;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Button buttonZoom;
+    }
+}
---
+++ BDX.MagicShapes/BDX.MagicShapes.UI/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BDX.MagicShapes.UI
+{
+    public partial class MainForm : Form
+    {
+        private Point currentPoint, mouseDownPoint, mouseUpPoint;
+        private bool zoomed;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonZoom_Click(object sender, EventArgs e)
+        {
+            zoomed = !zoomed;
+            if (zoomed)
+            {
+                buttonZoom.Text = "Zoom Out";
+            }
+            else
+            {
+                buttonZoom.Text = "Zoom In";
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentPoint = canvas.PointToClient(Cursor.Position);
+            //ToDo... Here the rectangle should be temporarly drawed but only when mouse button is hold
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownPoint = canvas.PointToClient(Cursor.Position);
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseUpPoint = canvas.PointToClient(Cursor.Position);
+            drawRectangle();
+        }
+
+        private void drawRectangle()
+        {
+            Point locationPoint = new Point(Math.Min(mouseDownPoint.X, mouseUpPoint.X), Math.Min(mouseDownPoint.Y, mouseUpPoint.Y));
+            Graphics graphics = canvas.CreateGraphics();
+            Pen pen = new Pen(Color.Olive, 2);
+            SolidBrush brush = new SolidBrush(Color.FromArgb(128, 0, 0, 255));
+            int width = Math.Abs(mouseDownPoint.X - mouseUpPoint.X);
+            int height = Math.Abs(mouseDownPoint.Y - mouseUpPoint.Y);
+            Rectangle rectangle = new Rectangle(locationPoint.X, locationPoint.Y, width, height);
+            graphics.FillRectangle(brush, rectangle);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+    }
+}
